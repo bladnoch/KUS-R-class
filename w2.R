35ead37afa8a553c7aded1fccb6c17db2c696fab
DK <dongwook5478@gmail.com>
2023-09-13 15:44:20 +0900

    rename w2 file to week2-inclass

--- w2.R
+++
@@ -1,8 +0,0 @@
-
-# setting working dir
-
-getwd()
-
-WORK_DIR <- Users/doungukkim/Desktop/workspace/KUS-R-class
-WORK_DIR
-setwd(WORK_DIR)
---
+++ week2-inclass.R
@@ -0,0 +1,22 @@
+#this line has to be on the top of the file
+setRepositories()
+
+# setting working dir
+getwd()
+
+WORK_DIR <- Users/doungukkim/Desktop/workspace/KUS-R-class
+WORK_DIR
+setwd(WORK_DIR)
+
+
+#giving values
+a <- 10
+
+
+
+#getting package manual
+help("cbind")
+
+#add package
+install.packages("")
+library()
