2f4b56b6fc7e7197ef10ac5ddc9fbeb4914f08a2
DK <dongwook5478@gmail.com>
2023-12-10 20:42:03 +0900

    add new HW3 files

---
+++ HW3/HW3.R
@@ -0,0 +1,15 @@
+##################################
+## Homework 3.                  ##
+## for Probability & Statistics ##
+## Dounguk Kim                  ##
+## 2023-11-22                   ##
+##################################
+
+
+setRepositories(ind = 1:7)
+
+getwd()
+
+WORK_DIR <- "/Users/doungukkim/Desktop/workspace/KUS-R-class/HW3"
+WORK_DIR
+setwd(WORK_DIR)
